708467ba418cfc740b687778d3b94a0e33e505ab
Tanishq <tanishq130805@gmail.com>
2025-05-13 05:05:15 +0530

    404 Page not found

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import VerifyPage from './pages/VerifyPage';
+import { PageNotFoundDemo } from './components/ui/demo';
+import { NotFoundPage } from './components/ui/404-page-not-found';
 
 // Restored animated components
 const App: React.FC = () => {
@@ -10,6 +12,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/verify" element={<VerifyPage />} />
+        <Route path="/demo/404" element={<PageNotFoundDemo />} />
+        <Route path="*" element={<NotFoundPage />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
